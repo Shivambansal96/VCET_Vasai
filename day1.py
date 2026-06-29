6867434aa731ca516c432cfab07e6b88bfc1753f
Shivam Bansal <bansal.shivam1216@gmail.com>
2026-06-29 12:56:27 +0530

    Day 1

---
+++ day1.py
@@ -0,0 +1,93 @@
+
+# # # print("My name is Shivam Bansal", "SAME LINE", sep=" || ")
+# # # print("Today is Day 1 of Python")
+
+
+# # # name = "Shivam"
+
+# # # n = None
+
+# # # arr = [3, 5, 8, 9, 7]
+# # # print(8 in arr)
+
+# # # a = 5
+# # # a = a + 1
+# # # # a += 1
+# # # print(a)
+
+
+# # # print("---------------------")
+
+
+# # # # print(2 == 2 and 4 == 4 and 3 == 4)
+# # # print(2 == 2 or 4 == 4 and 3 == 4)
+# # # print(not (2 == 2 or 4 == 4) and 3 == 4)
+
+# # # a = 2
+# # # b = "4"
+# # # c = int(b)
+# # # print(a + c)
+
+
+# # name = input("Enter you name = ")
+# # marks = int(input("Enter your marks = "))
+
+# # print("Name =", name)
+# # print(type(name))
+# # print("------------")
+# # print("Marks =", marks)
+# # print(type(marks))
+
+# # print(f"My name is {name} and I got {marks} marks now.")
+# # print(f"My name is Shivam and I got 99 marks now.")
+
+
+
+# # # # # PRACTICE SET 2 # #  # # # #
+# # ============== SUM of 2 numbers ==================
+
+# a = int(input("Enter a number = "))
+# b = int(input("Enter another number = "))
+# print(f"Sum = {a + b}")
+
+# # ============== SUM of 2 numbers ==================
+# # ============== PRINT AREA ==================
+# side = int(input("Enter a side = "))
+
+# print("Area =",side ** 2)
+# # ============== PRINT AREA ==================
+# # ======= PRINT AVERAGE OF 2 FLOATING NUMS ==============
+# a = float(input("Enter a num: "))
+# b = float(input("Enter another num: "))
+
+# print("Average:", (a+b) / 2)
+# # ======= PRINT AVERAGE OF 2 FLOATING NUMS ==============
+# # ======= PRINT GREATER OF 2 ==============
+# a = int(input("Enter a num: "))
+# b = int(input("Enter another num: "))
+# print(a >= b)
+# # ======= PRINT GREATER OF 2 ==============
+
+# # ======= CONVERT CELSIUS TO FAHRENHEIT ==============
+# c = float(input("Enter the temperature: "))
+# f = (c * 9/5) + 32 
+# print(f"Temperature = {f}°F")
+# # ======= CONVERT CELSIUS TO FAHRENHEIT ==============
+
+
+a = "Shivam"
+b = "Bansal"
+
+print(a + " " + b)
+# print(len(a) + len(b))
+print(len(a + b))
+
+
+fName = input("Enter your first Name: ")
+
+print(f"Name = {fName}")
+print(f"length of {fName} = {len(fName)} characters.")
+
+
+
+# https://canva.link/du2keuj6ujr49yj
